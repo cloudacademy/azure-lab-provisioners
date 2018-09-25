762b34b9ca564f665090d71004bd248ad0bcb3df
Logan Rakai <lrakai@users.noreply.github.com>
2018-09-25 17:51:34 -0600

    Update deploy.sh

--- app-gw/deploy.sh
+++ app-gw/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce-18.06.1.ce-3.el7
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo docker run -e hostname=$(hostname) -p 80:80 --restart always -d lrakai/tetris:hostname
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce-18.06.1.ce-3.el7
+systemctl enable docker
+systemctl start docker
+docker run -e hostname=$(hostname) -p 80:80 --restart always -d lrakai/tetris:hostname
