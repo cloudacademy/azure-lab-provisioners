0337f4c752ab1eca156ef16884d54538834e2341
Logan Rakai <lrakai@users.noreply.github.com>
2018-09-25 23:15:57 -0600

    Add comments

--- app-gw/deploy.sh
+++ app-gw/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+# Install and start Docker
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce-18.06.1.ce-3.el7
 systemctl enable docker
 systemctl start docker
+# Run the web application and set the hostname environment variable
 docker run -e hostname=$(hostname) -p 80:80 --restart always -d lrakai/tetris:hostname
