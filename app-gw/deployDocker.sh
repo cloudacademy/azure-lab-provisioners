072a7a91a334e81e3d8845797f85e46f5b28bca7
Naz Rejepov <nazar.rejepov@qa.com>
2024-08-08 12:10:21 +0100

    add deploysDocker.sh

---
+++ app-gw/deployDocker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install Docker
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Enable and start Docker
+systemctl enable docker
+systemctl start docker
+
+# Run the web application and set the hostname environment variable
+docker run -e hostname=$(hostname) -p 80:80 --restart always -d lrakai/tetris:hostname
